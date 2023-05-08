9aee289d335135de419e6345d917f48f188fa5e0
Pol Maresch <polmaresch@gmail.com>
2023-05-08 19:15:29 +0200

    Colleccionable screen 80%

--- Output/Assets/Scripts/Coleccionable_buttons.h
+++ Output/Assets/Scripts/Coleccionable_buttons.h
@@ -2,7 +2,7 @@
 #include "API/HelloBehavior.h"
 #include "ScriptToInspectorInterface.h"
 #include "Macro.h"
-
+#include "../Scripts/Player/PlayerStorage.h"
 #include "API/API.h"
 
 class Coleccionable_buttons : HelloBehavior
@@ -11,9 +11,22 @@ public:
 	void Start() override; 
 	void Update() override;
 
-	API_UIButton button1;
-	API_UIButton button2;
-	API_UIButton button3;
+	void Level1selected(int level1, bool casete1, bool casete2,bool casete3);
+	void Level2selected(int level2, bool casete1, bool casete2,bool casete3);
+	void Level3selected(int level3, bool casete1, bool casete2,bool casete3);
+
+	API_UICheckBox CheckBox[3];
+
+	API_UIImage level_selected;
+
+	uint levels[3];
+	
+	API_GameObject Casetes[3];
+
+	API_GameObject playerStorageGO;
+	PlayerStorage* playerStorage;
+
+
 
 };
 

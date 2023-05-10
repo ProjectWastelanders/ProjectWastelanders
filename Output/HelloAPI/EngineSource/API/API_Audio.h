d4e14159b74adcaf066d2cd10e89ec864b56c29f
Adria Sellares <adsesa93@gmail.com>
2023-05-10 19:49:05 +0200

    Add button audio in MainMenu

--- Output/HelloAPI/EngineSource/API/API_Audio.h
+++ Output/HelloAPI/EngineSource/API/API_Audio.h
@@ -9,5 +9,6 @@ namespace API
 		static uint Event(const char* eventName);
 		static uint Event(uint eventID);
 		static void StopEvent(uint playID);
+		static void SetGlobalGameParameter(const char* parameterName, float value);
 	};
 }

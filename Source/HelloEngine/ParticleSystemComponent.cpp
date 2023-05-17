17ff1441ba176c9f193136e51cf6dfd1927e4b1e
David Benages <79161120+Divangus@users.noreply.github.com>
2023-05-17 11:51:39 +0200

    Particle Serialization

--- Source/HelloEngine/ParticleSystemComponent.cpp
+++ Source/HelloEngine/ParticleSystemComponent.cpp
@@ -502,9 +502,14 @@ void ParticleSystemComponent::Serialization(json& j)
 		_j["ParticleModules"]["ModuleMain"]["PlayOnAwake"] = ParticleEmitter.playOnAwake;
 		_j["ParticleModules"]["ModuleEmission"]["ParticlesPerSecond"] = ParticleEmitter.ParticlesPerSecond;
 		_j["ParticleModules"]["ModuleEmission"]["Enable"] = ParticleEmitter.enableEmissionModule;
-		_j["ParticleModules"]["ShapeModule"]["ShapeType"] = GetCurrentShape()->shapeType;
-		
-		GetCurrentShape()->Serialization(_j);
+
+		if (GetCurrentShape() == nullptr) {
+			_j["ParticleModules"]["ShapeModule"]["ShapeType"] = ShapeType::NONE;
+		}
+		else {
+			_j["ParticleModules"]["ShapeModule"]["ShapeType"] = GetCurrentShape()->shapeType;
+			GetCurrentShape()->Serialization(_j);
+		}		
 	}
 	_j["ParticleVectorSize"] = size;
 	_j["Enabled"] = _isEnabled;
@@ -559,14 +564,14 @@ void ParticleSystemComponent::DeSerialization(json& j)
 	ParticleEmitter.playOnAwake = j["ParticleModules"]["ModuleMain"]["PlayOnAwake"];
 	ParticleEmitter.loop = j["ParticleModules"]["ModuleMain"]["Looping"];
 	ParticleEmitter.ParticlesPerSecond = j["ParticleModules"]["ModuleEmission"]["ParticlesPerSecond"];
-	//ParticleEmitter.enableEmissionModule = j["ParticleModules"]["ModuleEmission"]["Enable"];
-	//size = j["ParticleVectorSize"];
-	//sizeCpy = j["ParticleVectorSize"];
-	/*ShapeType shape = j["ParticleModules"]["ShapeModule"]["ShapeType"];
+	ParticleEmitter.enableEmissionModule = j["ParticleModules"]["ModuleEmission"]["Enable"];
+	size = j["ParticleVectorSize"];
+	sizeCpy = j["ParticleVectorSize"];
+	ShapeType shape = j["ParticleModules"]["ShapeModule"]["ShapeType"];
 	CreateCurrentShape(shape);
 	if (shape != ShapeType::NONE) {
 		GetCurrentShape()->DeSerialization(j);
-	}*/
+	}
 	
 	bool enabled = j["Enabled"];
 

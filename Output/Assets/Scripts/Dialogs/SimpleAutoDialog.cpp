f2eaf465e38ec5b783662784601486595b34121f
David Benages <79161120+Divangus@users.noreply.github.com>
2023-05-18 19:14:17 +0200

    SimpleAutoDialog

---
+++ Output/Assets/Scripts/Dialogs/SimpleAutoDialog.cpp
@@ -0,0 +1,68 @@
+#include "SimpleAutoDialog.h"
+HELLO_ENGINE_API_C SimpleAutoDialog* CreateSimpleAutoDialog(ScriptToInspectorInterface* script)
+{
+	SimpleAutoDialog* classInstance = new SimpleAutoDialog();
+	//Show variables inside the inspector using script->AddDragInt("variableName", &classInstance->variable);
+
+	script->AddDragBoxUIImage("Dialog 1", &classInstance->Dialog);
+
+	script->AddDragFloat("Dialog timer", &classInstance->timer);
+
+	return classInstance;
+}
+
+void SimpleAutoDialog::Start()
+{
+	activeDialog = false;
+
+	initalPos = { 0, -1.500, 0 };
+	movingPos = { 0, -1.500, 0 };
+	finalPos = { 0, -0.500, 0 };
+
+	Dialog.GetGameObject().GetTransform().SetPosition(initalPos);
+	Dialog.GetGameObject().SetActive(false);
+}
+void SimpleAutoDialog::Update()
+{
+	if (activeDialog) {
+		PrintDialog(Dialog);
+	}
+}
+
+void SimpleAutoDialog::PrintDialog(API_UIImage& Dialog)
+{
+    Dialog.GetGameObject().SetActive(true);
+
+    if (_timer >= timer) {
+        if (Dialog.GetGameObject().GetTransform().GetGlobalPosition().y > initalPos.y)
+        {
+            movingPos.y -= 1 * Time::GetDeltaTime();
+        }
+        else {
+            Dialog.GetGameObject().SetActive(false);
+           /* activeDialog = false;
+            _timer = 0;*/
+        }
+    }
+    else {
+        if (Dialog.GetGameObject().GetTransform().GetGlobalPosition().y < finalPos.y)
+        {
+            movingPos.y += 1 * Time::GetDeltaTime();
+        }
+        else
+        {
+            _timer += Time::GetDeltaTime();
+        }
+    }
+
+    Dialog.GetGameObject().GetTransform().SetPosition(movingPos);
+}
+
+void SimpleAutoDialog::OnCollisionEnter(API::API_RigidBody other)
+{
+    std::string detectionTag = other.GetGameObject().GetTag();
+    if (detectionTag == "Player")
+    {
+        activeDialog = true;
+    }
+}
---
+++ Output/Assets/Scripts/Dialogs/SimpleAutoDialog.h
@@ -0,0 +1,27 @@
+#pragma once
+#include "API/HelloBehavior.h"
+#include "ScriptToInspectorInterface.h"
+#include "Macro.h"
+
+#include "API/API.h"
+
+class SimpleAutoDialog : HelloBehavior
+{
+public:
+	void Start() override; 
+	void Update() override;
+	void PrintDialog(API_UIImage& Dialog);
+	void OnCollisionEnter(API::API_RigidBody other) override;
+
+	API_UIImage Dialog;
+
+	API_Vector3 initalPos;
+	API_Vector3 finalPos;
+	API_Vector3 movingPos;
+
+	bool activeDialog;
+
+	float timer = 0.0f;
+	float _timer = 0.0f;
+};
+

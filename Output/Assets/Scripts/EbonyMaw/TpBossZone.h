482fd2f49fc6c024c9c01f675da31b91790b428f
Loproxi <79161178+Loproxi@users.noreply.github.com>
2023-05-21 11:45:09 +0200

    Boss Tp Collider bug solved

---
+++ Output/Assets/Scripts/EbonyMaw/TpBossZone.h
@@ -0,0 +1,21 @@
+#pragma once
+#include "API/HelloBehavior.h"
+#include "ScriptToInspectorInterface.h"
+#include "Macro.h"
+
+#include "API/API.h"
+
+class BossLoop;
+
+class TpBossZone : HelloBehavior
+{
+public:
+	void Start() override; 
+	void Update() override;
+
+	API_GameObject bossGO;
+	BossLoop* bossloopref;
+
+	API_GameObject collider;
+};
+

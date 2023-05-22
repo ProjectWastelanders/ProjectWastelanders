654dd4cc00dfd19cec4fe56fca86464d99bc56e9
MaralGS <79161220+MaralGS@users.noreply.github.com>
2023-05-22 14:17:34 +0200

    Ability Tutorial Start(Not Finished)

--- Output/Assets/Scripts/AbilityTreeScript.cpp
+++ Output/Assets/Scripts/AbilityTreeScript.cpp
@@ -21,6 +21,11 @@ HELLO_ENGINE_API_C AbilityTreeScript* CreateAbilityTreeScript(ScriptToInspectorI
     script->AddDragBoxGameObject("PanelAbility4", &classInstance->PanelAbility[3]);
 
     script->AddDragBoxGameObject("Player", &classInstance->playerGO);
+
+    script->AddDragBoxUIImage("Tutorial 1", &classInstance->Tutorial_1);
+    script->AddDragBoxUIImage("Tutorial 2", &classInstance->Tutorial_2);
+    script->AddDragBoxUIImage("Tutorial 3", &classInstance->Tutorial_3);
+
     return classInstance;
 }
 
@@ -38,73 +43,88 @@ void AbilityTreeScript::Start()
 
 void AbilityTreeScript::Update()
 {
-    if (Input::GetGamePadButton(GamePadButton::BUTTON_B) == KeyState::KEY_DOWN && mainPanel.IsEnabled())
-    {
-        Audio::Event("click");
-        if (playerMove) playerMove->openingChest = false;
-        mainPanel.GetGameObject().SetActive(false);
-        for (int i = 0; i < 4; ++i)
+    if (tutorialActive == false) {
+        if (Input::GetGamePadButton(GamePadButton::BUTTON_B) == KeyState::KEY_DOWN && mainPanel.IsEnabled())
         {
-            PanelAbility[i].SetActive(false);
+            Audio::Event("click");
+            if (playerMove) playerMove->openingChest = false;
+            mainPanel.GetGameObject().SetActive(false);
+            for (int i = 0; i < 4; ++i)
+            {
+                PanelAbility[i].SetActive(false);
+            }
+            HUB_UIManager::ClosePanel();
+            return;
         }
-        HUB_UIManager::ClosePanel();
-        return;
-    }
 
-    if (!mainPanel.GetGameObject().IsActive()) return;
+        if (!mainPanel.GetGameObject().IsActive()) return;
 
-    if (AbilityList1.OnHovered())
-    {
-        if (ability1)
+        if (AbilityList1.OnHovered())
         {
-            Audio::Event("hover");
-            ability1 = false;
-            ability2 = true;
+            if (ability1)
+            {
+                Audio::Event("hover");
+                ability1 = false;
+                ability2 = true;
+            }
+            PanelAbility[0].SetActive(true);
+            PanelAbility[1].SetActive(false);
+            PanelAbility[2].SetActive(false);
+            PanelAbility[3].SetActive(false);
         }
-        PanelAbility[0].SetActive(true);
-        PanelAbility[1].SetActive(false);
-        PanelAbility[2].SetActive(false);
-        PanelAbility[3].SetActive(false);
+        else if (AbilityList2.OnHovered())
+        {
+            if (ability2)
+            {
+                Audio::Event("hover");
+                ability2 = false;
+                ability3 = true;
+                ability1 = true;
+            }
+            PanelAbility[0].SetActive(false);
+            PanelAbility[1].SetActive(true);
+            PanelAbility[2].SetActive(false);
+            PanelAbility[3].SetActive(false);
+        }
+        else if (AbilityList3.OnHovered())
+        {
+            if (ability3)
+            {
+                Audio::Event("hover");
+                ability3 = false;
+                ability2 = true;
+                ability4 = true;
+            }
+            PanelAbility[0].SetActive(false);
+            PanelAbility[1].SetActive(false);
+            PanelAbility[2].SetActive(true);
+            PanelAbility[3].SetActive(false);
+        }
+        else if (AbilityList4.OnHovered())
+        {
+            if (ability4)
+            {
+                Audio::Event("hover");
+                ability4 = false;
+                ability3 = true;
+            }
+            PanelAbility[0].SetActive(false);
+            PanelAbility[1].SetActive(false);
+            PanelAbility[2].SetActive(false);
+            PanelAbility[3].SetActive(true);
+        }
     }
-    else if (AbilityList2.OnHovered())
+    else if (tutorialActive == true) 
     {
-        if (ability2)
+        if (tutorial1 == true) 
         {
-            Audio::Event("hover");
-            ability2 = false;
-            ability3 = true;
-            ability1 = true;
+            Tutorial_1.GetGameObject().SetActive(true);
+            if (Input::GetGamePadButton(GamePadButton::BUTTON_A) == KeyState::KEY_DOWN)
+            {
+                Tutorial_1.GetGameObject().SetActive(false);
+                tutorial2 = true;
+                tutorial1 = false;
+            }
         }
-        PanelAbility[0].SetActive(false);
-        PanelAbility[1].SetActive(true);
-        PanelAbility[2].SetActive(false);
-        PanelAbility[3].SetActive(false);
-    }
-    else if (AbilityList3.OnHovered())
-    {
-        if (ability3)
-        {
-            Audio::Event("hover");
-            ability3 = false;
-            ability2 = true;
-            ability4 = true;
-        }
-        PanelAbility[0].SetActive(false);
-        PanelAbility[1].SetActive(false);
-        PanelAbility[2].SetActive(true);
-        PanelAbility[3].SetActive(false);
-    }
-    else if (AbilityList4.OnHovered())
-    {
-        if (ability4)
-        {
-            Audio::Event("hover");
-            ability4 = false;
-            ability3 = true;
-        }
-        PanelAbility[0].SetActive(false);
-        PanelAbility[1].SetActive(false);
-        PanelAbility[2].SetActive(false);
-        PanelAbility[3].SetActive(true);
     }
 }

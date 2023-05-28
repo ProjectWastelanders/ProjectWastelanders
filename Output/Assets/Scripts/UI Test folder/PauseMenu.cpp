64930f60903adf3cc362afe95ec46cda63ed5dff
MaralGS <algarselma@gmail.com>
2023-05-29 01:13:46 +0200

    Player don't aim when you pause

--- Output/Assets/Scripts/UI Test folder/PauseMenu.cpp
+++ Output/Assets/Scripts/UI Test folder/PauseMenu.cpp
@@ -1,11 +1,14 @@
 #include "PauseMenu.h"
 #include "UIManager.h"
 #include "HUB/HUB_UIManager.h"
+#include "../Player/PlayerMove.h"
+
 HELLO_ENGINE_API_C PauseMenu* CreatePauseMenu(ScriptToInspectorInterface* script)
 {
 	PauseMenu* classInstance = new PauseMenu();
 	//Show variables inside the inspector using script->AddDragInt("variableName", &classInstance->variable);
 	script->AddDragBoxGameObject("UI Manager: ", &classInstance->uiManager);
+	script->AddDragBoxGameObject("Player", &classInstance->player);
 	script->AddDragBoxUIButton("Continue: ", &classInstance->continueButton);
 	script->AddDragBoxUIButton("Settings: ", &classInstance->settingsButton);
 	script->AddDragBoxUIButton("Main Menu: ", &classInstance->mainMenuButton);
@@ -16,9 +19,13 @@ HELLO_ENGINE_API_C PauseMenu* CreatePauseMenu(ScriptToInspectorInterface* script
 void PauseMenu::Start()
 {
 	manager = (UIManager*)uiManager.GetScript("UIManager");
+	blockMovement = (PlayerMove*)player.GetScript("PlayerMove");
+	
+	
 }
 void PauseMenu::Update()
 {
+	blockMovement->openingChest = true;
 	if (continueButton.OnPress())
 	{
 		manager->ContinueGame();
@@ -26,6 +33,7 @@ void PauseMenu::Update()
 	else if (settingsButton.OnPress())
 	{
 		manager->ShowSettings();
+		blockMovement->openingChest = true;
 	}
 	else if (mainMenuButton.OnPress())
 	{
@@ -33,4 +41,8 @@ void PauseMenu::Update()
 		Time::ChangeTimeScale(1.0f);
 		Scene::LoadScene("MainMenu.HScene");
 	}
+}
+void PauseMenu::OnDisable() 
+{
+	blockMovement->openingChest = false;
 }
--- Output/Assets/Scripts/UI Test folder/PauseMenu.h
+++ Output/Assets/Scripts/UI Test folder/PauseMenu.h
@@ -7,6 +7,7 @@
 #include"API/API_UIImage.h"
 
 class UIManager;
+class PlayerMove;
 
 class PauseMenu : HelloBehavior
 {
@@ -14,12 +15,16 @@ public:
 	void Start() override;
 	void Update() override;
 
+	void OnDisable() override;
+
 	API_GameObject uiManager;
+	API_GameObject player;
 
 	API_UIButton continueButton;
 	API_UIButton settingsButton;
 	API_UIButton mainMenuButton;
 	UIManager* manager;
+	PlayerMove* blockMovement;
 
 };
 

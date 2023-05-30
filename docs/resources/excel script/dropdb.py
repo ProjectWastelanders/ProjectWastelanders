d1eff8fc6ea006a77c47c69b5bd9956e012a301e
ryansony <60979696+isaac553876299@users.noreply.github.com>
2023-05-30 23:02:21 +0530

    little update
    TODO: fix excel-script name conventions and fix strings with new lines and upperlowercase

--- docs/resources/excel script/dropdb.py
+++ docs/resources/excel script/dropdb.py
@@ -3,28 +3,30 @@ import csv
 import json
 
 class MyCustomClass:
-    def __init__(self, name, role, photo, github, linkedin, brief, contributions):
+    def __init__(self, name, role, photo, email, github, linkedin, brief, contributions):
         self.name = name
         self.role = role
         self.photo = photo
+        self.email = email
         self.github = github
         self.linkedin = linkedin
         self.brief = brief
         self.contributions = contributions
 #
 data = []
-with open('team_data.csv', 'r') as file:
+with open('Test.csv', 'r') as file:
     reader = csv.DictReader(file)
     print(reader.fieldnames)
     for row in reader:
         entry=MyCustomClass(
-            name=row['name'],
-            role=row['role'],
-            photo=row['photo'],
-            github=row['github'],
-            linkedin=row['linkedin'],
-            brief=row['brief'],
-            contributions=row['contributions'],)
+            name=row['Nombre'],
+            role=row['Rol (Design, Art, Code, Production)'],
+            photo=row['Foto de perfil (nombre del archivo) Ejemplo: (Nombre_Apellido.png)'],
+            email=row['Xarxes socials: e-mail de contacte'],
+            github=row['Xarxes socials: github'],
+            linkedin=row['Xarxes socials: linkedin'],
+            brief=row['Comentari general (1 parÃ\xa0graf)'],
+            contributions=row['Tasques principals (4 punts)'],)
         data.append(entry.__dict__)
 with open('team_data.json', 'w', encoding='utf-8') as f:
     json.dump(data, f, ensure_ascii=False, indent=4)
@@ -32,7 +34,11 @@ with open('team_data.json', 'w', encoding='utf-8') as f:
 html = []
 def fill_html(member):
     entry = '''
-    
+            <div class="col span_1_of_2 mix entry4role box">
+                <img src="resources/img/entry4photo" alt="Name 2" class="team-member">
+                <li><a class="myBtn_multi">entry4name</a></li>
+                <span class="role">entry4role</span>
+            </div>
             <!-- The Modal 1-->
             <div class="modal modal_multi">
 
@@ -55,7 +61,7 @@ def fill_html(member):
                                 <ul>
                                     <li><a href="entry4github"><i class="fab fa-github"></i></a></li>
                                     <li><a href="entry4linkedin"><i class="fab fa-linkedin-in"></i></a></li>
-                                    <li><a href="#"><i class="fas fa-envelope"></i></a></li>
+                                    <li><a href="entry4email"><i class="fas fa-envelope"></i></a></li>
                                 </ul>
                             </div>
                         </div>
@@ -71,10 +77,12 @@ def fill_html(member):
     '''
     entry = entry.replace('entry4brief', member['brief'])
     entry = entry.replace('entry4tasks', member['contributions'])
+    entry = entry.replace('entry4email', member['email'])
     entry = entry.replace('entry4github', member['github'])
     entry = entry.replace('entry4linkedin', member['linkedin'])
     entry = entry.replace('entry4photo', member['photo'])
     entry = entry.replace('entry4name', member['name'])
+    entry = entry.replace('entry4role', member['role'])
     html.append(entry)
 #
 for member in data:

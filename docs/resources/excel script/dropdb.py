97d20b739d75ad000a635b221d2a8787f19f0b3a
ryansony <60979696+isaac553876299@users.noreply.github.com>
2023-06-02 05:23:06 +0530

    team members' card looking clean

--- docs/resources/excel script/dropdb.py
+++ docs/resources/excel script/dropdb.py
@@ -14,7 +14,7 @@ class MyCustomClass:
         self.contributions = contributions
 #
 data = []
-with open('test2.csv', 'r') as file:
+with open('test2.csv', 'r', encoding='utf-8') as file:
     reader = csv.DictReader(file)
     print(reader.fieldnames)
     for row in reader:
@@ -25,71 +25,77 @@ with open('test2.csv', 'r') as file:
             email=row['Xarxes socials: e-mail de contacte'],
             github=row['Xarxes socials: github'],
             linkedin=row['Xarxes socials: linkedin'],
-            brief=row['Comentari general (1 parÃ\xa0graf)'],
+            brief=row['Comentari general (1 paràgraf)'],
             contributions=row['Tasques principals (4 punts)'],)
         data.append(entry.__dict__)
-with open('team_data.json', 'w', encoding='utf-16') as f:
+with open('team_data.json', 'w', encoding='utf-8') as f:
     json.dump(data, f, ensure_ascii=False, indent=4)
 #
 html = []
 def fill_html(member):
+    entry4name = member['name'].replace('\n','')
+    entry4role = member['role'].replace('\n','').lower().replace(' ','').replace(',',' ')
+    entry4blueRole = 'Lead' if 'production' in entry4role else ''
+    entry4photo = 'resources/img/' + member['photo'].replace('\n','')
+    entry4email = member['email'].replace('\n','')
+    entry4github = member['github'].replace('\n','')
+    entry4linkedin = member['linkedin'].replace('\n','')
+    entry4brief = '\n'.join(['<p>\n'+p+'\n</p>' for p in member['brief'].split('\n') if p])
+    entry4contributions = '\n'.join(['<li><p>'+p+'</p></li>' for p in member['contributions'].split('\n') if p])
+    #
     entry = '''
-            <div class="col span_1_of_2 mix entry4role box">
-                <img src="resources/img/entry4photo" alt="Name 1" class="team-member">
-                <li><a class="myBtn_multi">entry4name</a></li>
-                <span class="role">entry4role</span>
-            </div>
-            <!-- The Modal 1-->
-            <div class="modal modal_multi">
-
-                <!-- Modal content -->
-                <div class="modal-content">
-                    <span class="close close_multi"></span>
-                    <div class="row">
-                        <div class="col span_1_of_2 box">
-                            <h4>General Info</h4>
-                            <p>entry4brief</p>
-
-                            <h4> Tasks Overview</h4>
-                            <ul>
-                                <li>
-                                    <p>entry4tasks</p>
-                                </li>
-                            </ul>
-
-                            <div class="social-link">
-                                <ul>
-                                    <li><a href="entry4github"><i class="fab fa-github"></i></a></li>
-                                    <li><a href="entry4linkedin"><i class="fab fa-linkedin-in"></i></a></li>
-                                    <li><a href="entry4email"><i class="fas fa-envelope"></i></a></li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div class="col span_1_of_2 box">
-                            <img src="resources/img/entry4photo" alt="Name 4" class="team-member">
-                            <h3>entry4name</h3>
-                        </div>
+    <div class="col span_1_of_2 mix entry4role box">
+        <img src="entry4photo" alt="Name 1" class="team-member">
+        <li><a class="myBtn_multi">entry4name</a></li>
+        <span class="role">entry4blueRole</span>
+    </div>
+    <div class="modal modal_multi">
+        <div class="modal-content">
+            <span class="close close_multi"></span>
+            <div class="row">
+                <div class="col span_1_of_2 box">
+                    <h4>General Info</h4>
+                    entry4brief
+                    <h4> Tasks Overview</h4>
+                    <ul>
+                        entry4contributions
+                    </ul>
+                </div>
+                <div class="col span_1_of_2 box">
+                    <img src="entry4photo" alt="Name 4" class="team-member">
+                    <h3>entry4name</h3>
+                    <span class="role">entry4role</span>
+                    <div class="social-link">
+                        <ul>
+                            <li><a href="entry4github"><i class="fab fa-github"></i></a></li>
+                            <li><a href="entry4linkedin"><i
+                                        class="fab fa-linkedin-in"></i></a></li>
+                        </ul>
+                        <span class="mail">entry4email</span>
                     </div>
                 </div>
-
             </div>
-
+        </div>
+    </div>
     '''
-    entry = entry.replace('entry4brief', member['brief'])
-    entry = entry.replace('entry4tasks', member['contributions'])
-    entry = entry.replace('entry4email', member['email'])
-    entry = entry.replace('entry4github', member['github'])
-    entry = entry.replace('entry4linkedin', member['linkedin'])
-    entry = entry.replace('entry4photo', member['photo'])
-    entry = entry.replace('entry4name', member['name'])
-    entry = entry.replace('entry4role', member['role'].lower().replace(",", "").replace(" ", " "))
+    #
+    entry = entry.replace('entry4name', entry4name)
+    entry = entry.replace('entry4role', entry4role)
+    entry = entry.replace('entry4blueRole', entry4blueRole)
+    entry = entry.replace('entry4photo', entry4photo)
+    entry = entry.replace('entry4email', entry4email)
+    entry = entry.replace('entry4github', entry4github)
+    entry = entry.replace('entry4linkedin', entry4linkedin)
+    entry = entry.replace('entry4brief', entry4brief)
+    entry = entry.replace('entry4contributions', entry4contributions)
+    #
     html.append(entry)
 #
 for member in data:
     fill_html(member)
 #
-with open('fillhtml.txt', 'w', encoding='utf-16') as file:
+with open('fill_html.html', 'w', encoding='utf-8') as file:
     content = '\n'.join(html)
     file.write(content)
 #
-print('done!')
+print('done! --remember to format pretty')
